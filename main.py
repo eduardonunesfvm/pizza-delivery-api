b52f06e4ffc58c6f5cb4206ba20386a636cb9cf3
edu <aeonszx2@gmail.com>
2025-12-30 19:40:43 -0300

    chore: configure environment variables and password hashing

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from auth_routes import auth_router
 from order_routes import order_router
 
 app.include_router(auth_router)
 app.include_router(order_router)
+
+# para rodar o codigo, execute uvicorn main:app --reload no terminal
+
+#endpoint:
+#dominio.com/pedidos
+
+#rest APIs
+# Get -> leitura/pegar
+# Post -> enviar/criar
+# Put/Patch -> edição
+# Delete -> deletar
