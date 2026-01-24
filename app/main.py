5d33adf07cf5513f45f0e88c1343becabcb365ba
edu <aeonszx2@gmail.com>
2026-01-24 20:42:32 -0300

    fix: add python-dotenv to requirements

--- app/main.py
+++ app/main.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
